1ba71635e00eb68fca807470949b52b25af7f57e
Vladislav <selies@mail.ru>
2022-01-18 03:51:51 +0000

    finnaly deployed STRAPI CMS to heroku

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,26 +1,18 @@
-const { parse } = require("pg-connection-string");
-
-module.exports = ({ env }) => {
-  const { host, port, database, user, password } = parse(env("DATABASE_URL"));
-
-  return {
-    defaultConnection: "default",
-    connections: {
-      default: {
-        connector: "bookshelf",
-        settings: {
-          client: "postgres",
-          host,
-          port,
-          database,
-          username: user,
-          password,
-          ssl: { rejectUnauthorized: false },
-        },
-        options: {
-          ssl: false,
-        },
+const parse = require('pg-connection-string').parse;
+const config = parse(process.env.DATABASE_URL);
+module.exports = ({ env }) => ({
+  connection: {
+    client: 'postgres',
+    connection: {
+      host: config.host,
+      port: config.port,
+      database: config.database,
+      user: config.user,
+      password: config.password,
+      ssl: {
+        rejectUnauthorized: false
       },
     },
-  };
-};
+    debug: false,
+  },
+});
