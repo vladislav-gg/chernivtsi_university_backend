5c339291731c99d09ef2217dcb405b37907c2b09
Vladislav <selies@mail.ru>
2022-01-22 03:23:33 +0000

    some fixes

--- config/plugins.js
+++ config/plugins.js
@@ -4,9 +4,9 @@ module.exports = ({ env }) => ({
       config: {
         provider: 'cloudinary',
         providerOptions: {
-          cloud_name: process.env.CLOUDINARY_NAME,
-          api_key: process.env.CLOUDINARY_KEY,
-          api_secret: process.env.CLOUDINARY_SECRET,
+            cloud_name: env('CLOUDINARY_NAME'),
+            api_key: env('CLOUDINARY_API_KEY'),
+            api_secret: env('CLOUDINARY_API_SECRET'),
         },
         actionOptions: {
           upload: {},
