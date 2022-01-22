6c287ecf224ff9e8bc5a90cc3b81ef2b25188da3
Vladislav <selies@mail.ru>
2022-01-22 03:31:38 +0000

    another attempt to upload images to cloudinary

--- config/plugins.js
+++ config/plugins.js
@@ -4,9 +4,9 @@ module.exports = ({ env }) => ({
       config: {
         provider: 'cloudinary',
         providerOptions: {
-            cloud_name: env('CLOUDINARY_NAME'),
-            api_key: env('CLOUDINARY_API_KEY'),
-            api_secret: env('CLOUDINARY_API_SECRET'),
+            cloud_name: "ddge3uldr",
+            api_key: "165839532821859",
+            api_secret: "rIWII_BYIz4mFE0FEYbpRS1-N-g",
         },
         actionOptions: {
           upload: {},
